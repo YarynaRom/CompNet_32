ed182a168acd50f2bff701f1b4db6f6986619fa7
Yaryna Romaniuk <yaryna.romaniuk.pm.2023@lpnu.ua>
2026-05-18 19:49:10 +0300

    Lab_3

---
+++ Lab3/Client/Client.cpp
@@ -0,0 +1,126 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <string>
+#include <thread>
+
+using namespace std;
+
+#define SERVER_ANNOUNCE_SLOT "\\\\.\\mailslot\\MainServerSlot"
+
+string targetPipe;
+
+void backgroundReader(HANDLE pipeHandle) {
+    char recvBuf[1024];
+    DWORD readBytes;
+    DWORD dataAvailable;
+
+    HANDLE consoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    for (;;) {
+        if (PeekNamedPipe(pipeHandle, NULL, 0, NULL, &dataAvailable, NULL)) {
+            if (dataAvailable > 0) {
+                if (ReadFile(pipeHandle, recvBuf, sizeof(recvBuf) - 1, &readBytes, NULL) && readBytes > 0) {
+                    recvBuf[readBytes] = '\0';
+
+                    CONSOLE_SCREEN_BUFFER_INFO info;
+                    GetConsoleScreenBufferInfo(consoleOut, &info);
+
+                    COORD startPos = info.dwCursorPosition;
+                    startPos.X = 0;
+
+                    DWORD charsWritten;
+                    FillConsoleOutputCharacterA(consoleOut, ' ', info.dwSize.X, startPos, &charsWritten);
+
+                    SetConsoleCursorPosition(consoleOut, startPos);
+                    cout << recvBuf << "\n>>> " << flush;
+                }
+            }
+            else {
+                Sleep(40);
+            }
+        }
+        else {
+            cout << "\n[CONNECTION LOST]\n";
+            exit(1);
+        }
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+
+    string myName;
+    cout << "Enter your nickname: ";
+    getline(cin, myName);
+
+    string mySlotPath = "\\\\.\\mailslot\\UserSlot_" + myName;
+    HANDLE localSlot = CreateMailslotA(mySlotPath.c_str(), 0, MAILSLOT_WAIT_FOREVER, NULL);
+    if (localSlot == INVALID_HANDLE_VALUE) {
+        cout << "Error creating mailbox!\n";
+        return 1;
+    }
+
+    HANDLE servSlot = CreateFileA(SERVER_ANNOUNCE_SLOT, GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (servSlot == INVALID_HANDLE_VALUE) {
+        cout << "Server not found. Check if it is up and running..\n";
+        return 1;
+    }
+
+    DWORD wroteBytes;
+    WriteFile(servSlot, mySlotPath.c_str(), mySlotPath.size() + 1, &wroteBytes, NULL);
+    CloseHandle(servSlot);
+
+    char addrBuf[512];
+    DWORD readCnt, sizeNext, msgAmount;
+
+    for (;;) {
+        BOOL status = GetMailslotInfo(localSlot, NULL, &sizeNext, &msgAmount, NULL);
+        if (!status) return 1;
+
+        if (msgAmount > 0) break;
+
+        cout << "Waiting for a response from the server...\n";
+        Sleep(600);
+    }
+
+    ReadFile(localSlot, addrBuf, sizeof(addrBuf) - 1, &readCnt, NULL);
+    addrBuf[readCnt] = '\0';
+    targetPipe = addrBuf;
+
+    cout << "Network found: " << targetPipe << endl;
+    CloseHandle(localSlot);
+
+    HANDLE commPipe;
+    for (;;) {
+        commPipe = CreateFileA(targetPipe.c_str(), GENERIC_READ | GENERIC_WRITE, 0, NULL, OPEN_EXISTING, 0, NULL);
+        if (commPipe != INVALID_HANDLE_VALUE) break;
+
+        if (GetLastError() != ERROR_PIPE_BUSY) return 1;
+        if (!WaitNamedPipeA(targetPipe.c_str(), 5000)) return 1;
+    }
+
+    DWORD pipeMode = PIPE_READMODE_MESSAGE;
+    SetNamedPipeHandleState(commPipe, &pipeMode, NULL, NULL);
+
+    cout << "Successfully connected\n>>> ";
+
+    thread reader(backgroundReader, commPipe);
+    reader.detach();
+
+    string outText;
+
+    for (;;) {
+        getline(cin, outText);
+
+        if (outText == "exit" || outText.empty()) break;
+
+        string finalMsg = "[" + myName + "]: " + outText;
+
+        WriteFile(commPipe, finalMsg.c_str(), finalMsg.size() + 1, &wroteBytes, NULL);
+        cout << ">>> " << flush;
+    }
+
+    CloseHandle(commPipe);
+    return 0;
+}
---
+++ Lab3/CompN_Lab3/CompN_Lab3.cpp
@@ -0,0 +1,132 @@
+﻿#include <windows.h>
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <mutex>
+#include <algorithm> 
+
+using namespace std;
+
+#define PIPE_PATH "\\\\.\\pipe\\LabNetPipe"
+#define MAIN_SLOT "\\\\.\\mailslot\\MainServerSlot"
+
+vector<HANDLE> activeUsers;
+mutex syncLock;
+
+// Функція розсилки повідомлень
+void sendToAll(const string& text, HANDLE sender) {
+    lock_guard<mutex> guard(syncLock);
+
+    for (HANDLE userPipe : activeUsers) {
+        if (userPipe == sender) continue;
+
+        DWORD bytesWritten;
+        WriteFile(userPipe, text.c_str(), text.length() + 1, &bytesWritten, NULL);
+    }
+}
+
+// Потік обробки підключеного користувача
+void ProcessConnection(HANDLE clientPipe) {
+    char msgBuf[1024];
+    DWORD readCount;
+    DWORD availableData;
+
+    for (;;) {
+        if (PeekNamedPipe(clientPipe, NULL, 0, NULL, &availableData, NULL)) {
+            if (availableData > 0) {
+                BOOL ok = ReadFile(clientPipe, msgBuf, sizeof(msgBuf) - 1, &readCount, NULL);
+                if (!ok || readCount == 0) break;
+
+                msgBuf[readCount] = '\0';
+                sendToAll(msgBuf, clientPipe);
+            }
+            else {
+                Sleep(45);
+            }
+        }
+        else {
+            break;
+        }
+    }
+
+    {
+        lock_guard<mutex> guard(syncLock);
+        activeUsers.erase(remove(activeUsers.begin(), activeUsers.end(), clientPipe), activeUsers.end());
+    }
+
+    DisconnectNamedPipe(clientPipe);
+    CloseHandle(clientPipe);
+    cout << "The user has disconnected\n";
+}
+
+// Потік для відстеження нових підключень через поштову скриньку
+void SlotObserver() {
+    HANDLE sHandle = CreateMailslotA(MAIN_SLOT, 0, MAILSLOT_WAIT_FOREVER, NULL);
+    if (sHandle == INVALID_HANDLE_VALUE) {
+        cout << "Initialization Mailslot error !\n";
+        return;
+    }
+
+    char tempBuf[256];
+    DWORD cbRead;
+
+    for (;;) {
+        if (ReadFile(sHandle, tempBuf, sizeof(tempBuf) - 1, &cbRead, NULL)) {
+            tempBuf[cbRead] = '\0';
+            cout << "Connection request from: " << tempBuf << endl;
+
+            HANDLE outSlot = CreateFileA(
+                tempBuf, GENERIC_WRITE, FILE_SHARE_READ, NULL,
+                OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL
+            );
+
+            if (outSlot != INVALID_HANDLE_VALUE) {
+                DWORD cbWritten;
+                WriteFile(outSlot, PIPE_PATH, strlen(PIPE_PATH) + 1, &cbWritten, NULL);
+                CloseHandle(outSlot);
+            }
+            else {
+                cout << "Unable to respond to customer.\n";
+            }
+        }
+    }
+}
+
+int main() {
+    SetConsoleOutputCP(1251);
+    cout << "Server started\n";
+
+    thread observer(SlotObserver);
+    observer.detach();
+
+    for (;;) {
+        HANDLE pHandle = CreateNamedPipeA(
+            PIPE_PATH,
+            PIPE_ACCESS_DUPLEX,
+            PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+            PIPE_UNLIMITED_INSTANCES,
+            1024, 1024, 0, NULL
+        );
+
+        if (pHandle == INVALID_HANDLE_VALUE) {
+            Sleep(150);
+            continue;
+        }
+
+        BOOL isConnected = ConnectNamedPipe(pHandle, NULL) ? TRUE : (GetLastError() == ERROR_PIPE_CONNECTED);
+
+        if (isConnected) {
+            {
+                lock_guard<mutex> guard(syncLock);
+                activeUsers.push_back(pHandle);
+            }
+            thread worker(ProcessConnection, pHandle);
+            worker.detach();
+        }
+        else {
+            CloseHandle(pHandle);
+        }
+    }
+    return 0;
+}
